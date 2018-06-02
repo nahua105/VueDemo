a8feeeead67ceb10635f09a16915177d0e5ee65d
nahua105 <30335791+nahua105@users.noreply.github.com>
2018-06-02 15:37:29 +0800

    vue框架搭建

---
+++ Ty.FrontDemo/src/components/books/books.vue
@@ -0,0 +1,8 @@
+<template>
+ <div>
+    需要学习的书：{{bookName}} 
+  </div> 
+</template>
+
+<script src="./books.ts"></script>
+<style src="./books.css"></style>
--- Ty.FrontDemo/src/components/test/test.vue
+++ Ty.FrontDemo/src/components/test/test.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="A">
-    需要学习的书：{{bookName}} 
-  </div>
+    <Carousel
+        v-model="value3"
+        :autoplay="setting.autoplay"
+        :autoplay-speed="setting.autoplaySpeed"
+        :dots="setting.dots"
+        :radius-dot="setting.radiusDot"
+        :trigger="setting.trigger"
+        :arrow="setting.arrow">
+        <CarouselItem>
+            <div class="demo-carousel">
+              <img class="imgshow" src="./../../assets/desk1.jpg" alt="">
+            </div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">
+              <img class="imgshow" src="./../../assets/desk2.jpg" alt="">
+            </div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">
+              <img class="imgshow" src="./../../assets/desk3.jpg" alt="">
+            </div>
+        </CarouselItem>
+        <CarouselItem>
+              <img class="imgshow" src="./../../assets/desk4.jpg" alt="">
+            <div class="demo-carousel"></div>
+        </CarouselItem>
+    </Carousel>
 </template>
+  
 
 <script src="./test.ts"></script>
 <style src="./test.css"></style>
